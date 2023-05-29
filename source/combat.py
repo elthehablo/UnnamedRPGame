db6a1b242151b8567f3d9d0b5a64b833437c50ae
elthehablo <elthehablo3@gmail.com>
2023-05-29 17:02:31 +0200

    add WIP combat
    -added combat class with some combat properties

---
+++ source/combat.py
@@ -0,0 +1,79 @@
+import os
+
+from importer import *
+import keyregistry
+
+class Combat:
+    #contains values of the imported creatures
+    creatureValues = []
+    playerValues = []
+    currentCreatureHealth = 0
+    currentPlayerHealth = 0
+    
+    def __init__(self, combatPlayer, creatureID):
+        self.combatPlayer = combatPlayer
+        self.creatureID = creatureID
+        
+        #initialisation of creature and player
+        creatureValues = self.importCreature()
+        self.currentCreatureHealth = creatureValues[3]
+        self.currentPlayerHealth = combatPlayer[2]
+    
+    def importCreature(self):
+        creatureValues = ImportHandler.ImportCreature(self.creatureID)
+        return creatureValues
+    
+    def importPlayer(self, filename):
+        #imports stats for the player
+        #note: shouldn't really be used. just as a backup
+        #save files don't really include the relevant information
+        return
+    
+    
+    def printCombat(self):
+        keyBeingPressed = None
+        position = 0
+        while(keyBeingPressed != "quit"):
+            os.system('cls')
+            print("------------------")
+            print("A "+str(self.creatureValues[1].decode('UTF-8'))+" appears!")
+            print("------------------")
+            print("Creature level: "+str(self.creatureValues[2]))
+            print("Creature health: "+str(self.currentCreatureHealth)+"/"+str(self.creatureValues[3]))
+            print("------------------")
+            self.combatCursor(position)
+            keyBeingPressed = keyregistry.KeyRegistry.keyPressed()
+            if(keyBeingPressed == 'moveDown'):
+                if(position < 3):
+                    position += 1
+            elif(keyBeingPressed == 'moveUp'):
+                if(position > 0):
+                    position -= 1
+            elif(keyBeingPressed == "interact"):
+                if(position == 0):
+                    #TODO: implement fighting
+                    print("to be implemented")
+                elif(position == 1):
+                    #TODO: implement using ability (spells)
+                    print("to be implemented")
+                elif(position == 2):
+                    #TODO: implement using item
+                    print("to be implemented")
+                elif(position == 3):
+                    break
+                    
+        
+    def combatCursor(self, position):
+        print("What do you want to do?")
+        if(position == 0):
+            print(">", end = '')
+        print("fight")
+        if(position == 1):
+            print(">", end = '')
+        print("ability")
+        if(position == 2):
+            print(">", end = '')
+        print("item")
+        if(position == 3):
+            print(">", end = '')
+        print("run")
