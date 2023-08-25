1d2f1c92709ce92a037dd303bc0198993ba213c1
elthehablo <elthehablo3@gmail.com>
2023-08-25 11:33:58 +0200

    add better and more docs

--- source/documentation.py
+++ source/documentation.py
@@ -1,11 +1,26 @@
 import combat
+import dieroller
+import drawmap
+import importer
 
 class Documentation:
     @staticmethod
     def readDocumentation(entry):
-        match(entry):
-            case "Combat":
-                print(help(combat))
-            case _:
-                print("Not a valid docstring")
+        while(entry != "exit"):
+            match(entry):
+                case "combat":
+                    print(help(combat))
+                case "dieroller":
+                    print(help(dieroller))
+                case "drawmap":
+                    print(help(drawmap))
+                case "importer":
+                    print(help(importer))
+                case "help":
+                    print("Current classes with documentation: ")
+                    print("combat, dieroller, drawmap, importer")
+                case "exit":
+                    break
+                case _:
+                    print("Not a valid docstring")
         exit(0)
