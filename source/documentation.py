3858041bbba4c32e61ce92604b4bdc14b077cd53
elthehablo <elthehablo3@gmail.com>
2023-08-25 11:35:20 +0200

    fix

--- source/documentation.py
+++ source/documentation.py
@@ -6,21 +6,19 @@ import importer
 class Documentation:
     @staticmethod
     def readDocumentation(entry):
-        while(entry != "exit"):
-            match(entry):
-                case "combat":
-                    print(help(combat))
-                case "dieroller":
-                    print(help(dieroller))
-                case "drawmap":
-                    print(help(drawmap))
-                case "importer":
-                    print(help(importer))
-                case "help":
-                    print("Current classes with documentation: ")
-                    print("combat, dieroller, drawmap, importer")
-                case "exit":
-                    break
-                case _:
-                    print("Not a valid docstring")
-        exit(0)
+        match(entry):
+            case "combat":
+                print(help(combat))
+            case "dieroller":
+                print(help(dieroller))
+            case "drawmap":
+                print(help(drawmap))
+            case "importer":
+                print(help(importer))
+            case "help":
+                print("Current classes with documentation: ")
+                print("combat, dieroller, drawmap, importer")
+            case "exit":
+                exit(0)
+            case _:
+                print("Not a valid docstring")
